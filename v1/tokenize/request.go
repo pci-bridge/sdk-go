bf6d92869e04bf5d41bef010b8a195bf2fa3c375
Tom Kay <oridan82@gmail.com>
2022-05-05 17:07:49 +0100

    add nameOnCard to tokenize response (#3)

--- v1/tokenize/request.go
+++ v1/tokenize/request.go
@@ -1,7 +1,5 @@
 package tokenize
 
-import "github.com/pci-bridge/sdk-go/pcib"
-
 type Request struct {
 	MHID string `json:"mhid"`
 	RQID string `json:"rqid"`
@@ -37,20 +35,4 @@ type SessionData struct {
 	UserAgent          string    `json:"userAgent"`
 	CookiesEnabled     bool      `json:"cookiesEnabled"`
 	IsTouch            bool      `json:"isTouch"`
-}
-
-type Response struct {
-	TSID           string       `json:"tsid"`
-	TSIDK          string       `json:"tsidk"`
-	TokenID        string       `json:"tokenId"`
-	Token          string       `json:"token"`
-	TokenExpiry    int64        `json:"tokenExpiry"`
-	EphemeralToken string       `json:"ephemeralToken"`
-	Bin            string       `json:"bin"`
-	Last4          string       `json:"last4"`
-	ExpiryMonth    int32        `json:"expiryMonth"`
-	ExpiryYear     int32        `json:"expiryYear"`
-	PFP            string       `json:"pfp"`
-	MFP            string       `json:"mfp"`
-	BinData        pcib.BinData `json:"binData"`
 }
