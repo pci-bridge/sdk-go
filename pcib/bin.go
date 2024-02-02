913e7cc88442707fcdb7dd14dbfc51d0593d5691
Brooke Bryan <brooke@bajb.net>
2024-02-02 13:21:06 +0000

    Additional bin data

--- pcib/bin.go
+++ pcib/bin.go
@@ -1,13 +1,31 @@
 package pcib
 
 type BinData struct {
-	Scheme      string `json:"scheme"`
-	Brand       string `json:"brand"` // e.g. Visa Electron
-	SubType     string `json:"subType"`
-	CardType    string `json:"cardType"`
-	FundingType string `json:"fundingType"`
-	Issuer      Issuer `json:"issuer"`
-	Business    bool   `json:"business"`
+	Scheme      Scheme  `json:"scheme"`
+	Brand       string  `json:"brand"` // e.g. Visa Electron
+	SubType     string  `json:"subType"`
+	CardType    string  `json:"cardType"`
+	FundingType Funding `json:"fundingType"`
+	Issuer      Issuer  `json:"issuer"`
+
+	Business bool `json:"business"`
+	Consumer bool `json:"consumer"`
+
+	Length        int    `json:"length"`
+	PrePaid       bool   `json:"prePaid"`
+	Reloadable    bool   `json:"reloadable"`
+	NetworkToken  bool   `json:"networkToken"`
+	Refreshable   bool   `json:"refreshable"`
+	Alm           bool   `json:"alm"` // Account Level Management
+	DomesticOnly  bool   `json:"domesticOnly"`
+	GamblingBlock bool   `json:"gamblingBlock"`
+	Level2        bool   `json:"level2"`
+	Level3        bool   `json:"level3"`
+	Currency      string `json:"currency"`
+	SharedBin     bool   `json:"sharedBin"`
+
+	AuthenticationRequired bool   `json:"authenticationRequired"`
+	AuthenticationName     string `json:"authenticationName"` // e.g. PSD2
 }
 
 type Issuer struct {
